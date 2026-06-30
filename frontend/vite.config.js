ea4702e0061f686d6746ad14be597be74e973492
Phil-notfound <joelpmanooj326@gmail.com>
2026-06-30 14:28:49 +0530

    small

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
     port: 8080,
+    host: '0.0.0.0', 
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
+        ws: true // Enables websocket proxying if nodemon/vite is hanging on handshake
       }
     }
   }
-})
