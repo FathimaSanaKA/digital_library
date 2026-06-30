e80a0f9e1bad28fe24f5e75be09349ecdfd6e0b2
Phil-notfound <joelpmanooj326@gmail.com>
2026-06-30 13:47:35 +0530

    small

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,6 @@ app.get('/*splat', (req, res) => {
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || '0.0.0.0';
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST,  () => {
   console.log(`Server running successfully on port ${PORT}`);
 }); 
